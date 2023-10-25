a6ddfe449833e7f34ba55d1b43da2b0219f601ae
Armin Bazarjani <abizzle@Armins-MacBook-Pro.local>
2023-10-25 10:28:51 -0700

    pushing changes

--- gym-env/gym_env/envs/maze_env.py
+++ gym-env/gym_env/envs/maze_env.py
@@ -9,19 +9,25 @@ from gymnasium.utils import seeding
 class MazeEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    def __init__(self, maze_file, enable_render):
-        # read maze_file
+    def __init__(self, maze_file, render_mode=None):
+        # Read maze_file
         self.maze = self._read_maze_file(maze_file=maze_file)
 
-        # enable render
-        self._enable_render = enable_render
+        # Check if render mode is valid and set render mode
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
 
-        # get important positions
-        self._start_loc = np.where(self.maze == 'S')
-        self._target_loc = np.where(self.maze == 'G')
+        # Get important positions
+        start_loc = np.where(self.maze == 'S')
+        target_loc = np.where(self.maze == 'G')
+
+        self._start_loc = np.array([start_loc[0][0], start_loc[1][0]])
+        self._target_loc = np.array([target_loc[0][0], target_loc[1][0]])
         self._agent_loc = self._start_loc
 
+        # Size of maze and pygame window
         self.num_rows, self.num_cols = self.maze.shape
+        self.window_size = 512
 
         # 4 possible actions: 0=up, 1=down, 2=right, 3=left
         self.action_space = spaces.Discrete(4)
@@ -30,8 +36,8 @@ class MazeEnv(gym.Env):
         # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
         self.observation_space = spaces.Dict(
             {
-                "agent": spaces.Box(low=np.array(0,0), high=np.array(self.num_rows-1, self.num_cols-1), shape=(2,), dtype=int),
-                "target": spaces.Box(low=np.array(0,0), high=np.array(self.num_rows-1, self.num_cols-1), shape=(2,), dtype=int),
+                "agent": spaces.Box(low=np.array([0,0]), high=np.array([self.num_rows-1, self.num_cols-1]), shape=(2,), dtype=int),
+                "target": spaces.Box(low=np.array([0,0]), high=np.array([self.num_rows-1, self.num_cols-1]), shape=(2,), dtype=int),
             }
         )
 
@@ -42,13 +48,9 @@ class MazeEnv(gym.Env):
             3: np.array([0, -1]),
         }
 
-        # Initialize Pygame
-        pygame.init()
-        self.cell_size = 125
-
-        # setting display size
-        if self._enable_render is True:
-            self.screen = pygame.display.set_mode((self.num_cols * self.cell_size, self.num_rows * self.cell_size))
+        # Only used for human-rendering
+        self.window = None
+        self.clock = None
 
     def reset(self, seed=None):
         # We need the following line to seed self.np_random
@@ -59,9 +61,15 @@ class MazeEnv(gym.Env):
         observation = self._get_obs()
         info = self._get_info()
 
+        if self.render_mode == "human":
+            self._render_frame()
+
         return observation, info
 
     def step(self, action):
+        """
+        One step in our environment given the action
+        """
         # Get direction
         direction = self._action_to_direction[action]
 
@@ -72,15 +80,17 @@ class MazeEnv(gym.Env):
         if self._is_valid_position(new_loc):
             self._agent_loc = new_loc
 
-        # Reward function
-        if np.array_equal(self._agent_loc, self._target_loc):
-            reward = 1.0
-            done = True
-        else:
-            reward = 0.0
-            done = False
+        # Check if terminated
+        terminated = np.array_equal(self._agent_loc, self._target_loc)
+        reward = 1 if terminated else 0  # Binary sparse rewards
+        
+        if self.render_mode == "human":
+            self._render_frame()
+        
+        observation = self._get_obs()
+        info = self._get_info()
 
-        return self._agent_loc, reward, done, {}
+        return observation, reward, terminated, False, info
     
     def _read_maze_file(self, maze_file):
         dir_path = os.path.dirname(os.path.abspath(__file__))
@@ -89,9 +99,15 @@ class MazeEnv(gym.Env):
         return np.load(file=rel_path)
 
     def _get_obs(self):
+        """
+        Observation, returns the agent and target positions
+        """
         return {"agent": self._agent_loc, "target":self._target_loc}
     
     def _get_info(self):
+        """
+        Information, returns the manhattan (L1) distance between agent and target.
+        """
         return {
             "distance": np.linalg.norm(
                 self._agent_loc - self._target_loc, ord=1
@@ -99,6 +115,9 @@ class MazeEnv(gym.Env):
         }
 
     def _is_valid_position(self, pos):
+        """
+        Checks if position is in bounds or if obstacle is hit
+        """
         row, col = pos
    
         # If agent goes out of the grid
@@ -108,36 +127,105 @@ class MazeEnv(gym.Env):
         # If the agent hits an obstacle
         if self.maze[row, col] == '1':
             return False
+        
         return True
 
     def render(self):
-        if self._enable_render is False:
-            return
-        # Clear the screen
-        self.screen.fill((255, 255, 255))  
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
+    
+    def _render_frame(self):
+        """
+        Renders a frame in pygame
+        """
+        if self.window is None and self.render_mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode(
+                (self.window_size, self.window_size)
+            )
+        if self.clock is None and self.render_mode == "human":
+            self.clock = pygame.time.Clock()
 
-        # Draw env elements one cell at a time
-        for row in range(self.num_rows):
-            for col in range(self.num_cols):
-                cell_left = col * self.cell_size
-                cell_top = row * self.cell_size
-            
-                try:
-                    print(np.array(self._agent_loc)==np.array([row,col]).reshape(-1,1))
-                except Exception as e:
-                    print('Initial state')
+        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas.fill((255, 255, 255))
+        pix_square_size = (
+            self.window_size / self.num_cols
+        )  # The size of a single grid square in pixels
 
-                if self.maze[row, col] == '1':  # Obstacle
-                    pygame.draw.rect(self.screen, (0, 0, 0), (cell_left, cell_top, self.cell_size, self.cell_size))
-                elif self.maze[row, col] == 'S':  # Starting position
-                    pygame.draw.rect(self.screen, (0, 255, 0), (cell_left, cell_top, self.cell_size, self.cell_size))
-                elif self.maze[row, col] == 'G':  # Target position
-                    pygame.draw.rect(self.screen, (255, 0, 0), (cell_left, cell_top, self.cell_size, self.cell_size))
+        # Draw target
+        pygame.draw.rect(
+            canvas,
+            (255, 0, 0),
+            pygame.Rect(
+                pix_square_size * self._target_loc,
+                (pix_square_size, pix_square_size),
+            ),
+        )
+        # Draw start
+        pygame.draw.rect(
+            canvas,
+            (0, 255, 0),
+            pygame.Rect(
+                pix_square_size * self._start_loc,
+                (pix_square_size, pix_square_size),
+            ),
+        )
+        # Draw the agent
+        pygame.draw.circle(
+            canvas,
+            (0, 0, 255),
+            (self._agent_loc + 0.5) * pix_square_size,
+            pix_square_size / 3,
+        )
+        # Draw horizontal lines
+        for y in range(self.num_rows + 1):
+            pygame.draw.line(canvas, 0, (0, y * pix_square_size),
+                             (self.window_size, y * pix_square_size))
 
-                if np.array_equal(np.array(self.current_pos), np.array([row, col]).reshape(-1,1)):  # Agent position
-                    pygame.draw.rect(self.screen, (0, 0, 255), (cell_left, cell_top, self.cell_size, self.cell_size))
+        # Draw vertical lines
+        for x in range(self.num_cols + 1):
+            pygame.draw.line(canvas, 0, (x * pix_square_size, 0),
+                             (x * pix_square_size, self.window_size))
+        
+        # Draw obstacles
+        obs = np.where(self.maze == '1')
+        for i in range(obs[0].size):
+            row = obs[0][i]
+            col = obs[1][i]
 
-        pygame.display.update()  # Update the display
+            cell_left = col * pix_square_size
+            cell_top = row * pix_square_size
+
+            # Draw Obstacle
+            if self.maze[row, col] == '1':  # Obstacle
+                pygame.draw.rect(
+                    canvas,
+                    (0, 0, 0),
+                    pygame.Rect(
+                        (cell_left, cell_top),
+                        (pix_square_size, pix_square_size),
+                    ),
+                )
+
+        if self.render_mode == "human":
+            # The following line copies our drawings from `canvas` to the visible window
+            self.window.blit(canvas, canvas.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
+
+            # We need to ensure that human-rendering occurs at the predefined framerate.
+            # The following line will automatically add a delay to keep the framerate stable.
+            self.clock.tick(self.metadata["render_fps"])
+        else:  # rgb_array
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+            )
+    
+    def close(self):
+        if self.window is not None:
+            pygame.display.quit()
+            pygame.quit()
     
 
 class MazeEnv5x5(MazeEnv):
@@ -150,8 +238,16 @@ class MazeEnvHairpin(MazeEnv):
 
 if __name__ == '__main__':
     # Test it out
-    env = MazeEnv(maze_file="hairpin_14x14.npy")
-    print(env)
+    env = MazeEnv(maze_file="maze2d_5x5.npy")
+    # env = MazeEnv(maze_file="hairpin_14x14.npy")
+    print(f"env: {env}")
+    print(f"start loc: {env._start_loc}, target loc: {env._target_loc}")
+    obs, info = env.reset()
+    print(f"Post reset obs: {obs}, info: {info}")
+    rand_action = env.action_space.sample()
+    print(f"Random action: {rand_action}")
+    obs, reward, term, _, info = env.step(rand_action)
+    print(f"Post step obs: {obs}, reward: {reward}, terminated: {term}, info: {info}")
     # obs = env.reset()
     # done = True
     # while done:
