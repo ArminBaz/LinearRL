92467f12f5fc8350739ae01e7d3cf162c4396c55
Armin Bazarjani <abizzle@Armins-MacBook-Pro.local>
2023-10-31 15:35:30 -0700

    modified linear rl code

--- src/linear_rl/linear_rl.py
+++ src/linear_rl/linear_rl.py
@@ -6,26 +6,29 @@ import gymnasium as gym
 import gym_env
 
 import config
-from ..utils import helper_functions
+from ..utils import load_maze, create_transition_matrix_mapping, get_transition_matrix
 
 ENV_NAME = config.ENV_NAME
 
 class LinearRL():
     def __init__(self, ENV_NAME):
-        self.env = helper_functions.load_maze(ENV_NAME)
+        self.env = load_maze(ENV_NAME)
+        self.maze = self.env.unwrapped.maze
+        self.mapping = create_transition_matrix_mapping(self.maze)
+        self.T = get_transition_matrix(self.env, self.mapping)
 
-    def get_expv(self, T, terminals):
+    def get_expv(self):
         """
         Uses equations from the paper to calculate exp(v*)
         """
         _lambda = config.LAMBDA
-        terminals = np.diag(T) == 1     # find terminal states
+        terminals = np.diag(self.T) == 1     # find terminal states
 
-        c = np.full(len(T), 1)
+        c = np.full(len(self.T), 1)
         c[terminals] = 0
         r = -c
 
-        L = np.diag(np.exp(c / _lambda)) - T
+        L = np.diag(np.exp(c / _lambda)) - self.T
 
         # Remove rows and columns corresponding to terminals
         L = np.delete(L, terminals, axis=0)
@@ -35,7 +38,7 @@ class LinearRL():
         M = np.linalg.inv(L)
 
         # Calculate P = T_{NT}
-        P = T[~terminals][:,terminals]
+        P = self.T[~terminals][:,terminals]
 
         # Calculate expr
         expr = np.exp(r[terminals] / _lambda)
@@ -52,17 +55,17 @@ class LinearRL():
 
         return expv
     
-    def maze_value(self, maze, expv, mapping):
+    def maze_value(self, expv):
         """
         Uses exp(v*) and the mapping to create a value function of the optimal value for each state of the maze.
         Replaces blocked locations with negative infinity.
         """
-        v_maze = np.zeros_like(maze)
+        v_maze = np.zeros_like(self.maze)
         for row in range(v_maze.shape[0]):
             for col in range(v_maze.shape[1]):
-                if maze[row, col] == "1":
+                if self.maze[row, col] == "1":
                     v_maze[row,col] = np.NINF
                     continue
-                v_maze[row,col] = round(np.log(expv[mapping[(row,col)]]), 2)
+                v_maze[row,col] = round(np.log(expv[self.mapping[(row,col)]]), 2)
         
         return v_maze
