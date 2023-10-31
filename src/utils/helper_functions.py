9f88624d0a66a0cdd31bc7bfd37a41653b89ee29
Armin Bazarjani <abizzle@Armins-MacBook-Pro.local>
2023-10-31 15:28:52 -0700

    updated helper functions and init file

--- src/utils/helper_functions.py
+++ src/utils/helper_functions.py
@@ -97,59 +97,6 @@ def get_transition_matrix(env, mapping):
 
     return T
 
-def get_expv(T, terminals):
-    """
-    Uses equations from the paper to calculate exp(v*)
-    """
-    _lambda = config.LAMBDA
-    terminals = np.diag(T) == 1     # find terminal states
-
-    c = np.full(len(T), 1)
-    c[terminals] = 0
-    r = -c
-
-    L = np.diag(np.exp(c / _lambda)) - T
-
-    # Remove rows and columns corresponding to terminals
-    L = np.delete(L, terminals, axis=0)
-    L = np.delete(L, terminals, axis=1)
-
-    # Calculate the inverse of L
-    M = np.linalg.inv(L)
-
-    # Calculate P = T_{NT}
-    P = T[~terminals][:,terminals]
-
-    # Calculate expr
-    expr = np.exp(r[terminals] / _lambda)
-
-    # Initialize expv as zeros
-    expv = np.zeros(r.shape)
-
-    # Calculate expv for non-terminal states
-    expv_N = M @ P @ expr
-    expv[~terminals] = expv_N
-
-    # Calculate expv for terminal states
-    expv[terminals] = np.exp(r[terminals] / _lambda)
-
-    return expv
-
-def maze_value(maze, expv, mapping):
-    """
-    Uses exp(v*) and the mapping to create a value function of the optimal value for each state of the maze.
-    Replaces blocked locations with negative infinity.
-    """
-    v_maze = np.zeros_like(maze)
-    for row in range(v_maze.shape[0]):
-        for col in range(v_maze.shape[1]):
-            if maze[row, col] == "1":
-                v_maze[row,col] = np.NINF
-                continue
-            v_maze[row,col] = round(np.log(expv[mapping[(row,col)]]), 2)
-    
-    return v_maze
-
 def split_transition_matrix(T, mapping, target_locs):
     """
     Splits our transition matrix into T_nn and T_nt
