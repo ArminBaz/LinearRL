633ee1e3c8f40dcda4fc6b88052508bd387b2fa9
Armin Bazarjani <abizzle@Armins-MBP.attlocal.net>
2023-11-17 20:00:49 -0800

    thank god for stable baselines3git add wrappers.py

--- src/utils/wrappers.py
+++ src/utils/wrappers.py
@@ -206,6 +206,7 @@ class MaxAndSkipEnv(gym.Wrapper):
         self._obs_buffer.append(obs)
         return obs
 
+
 class NoopResetEnv(gym.Wrapper):
     def __init__(self, env=None, noop_max=30):
         """Sample initial states by taking random number of no-ops on reset.
